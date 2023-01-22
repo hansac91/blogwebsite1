12f250cee3053bcfa34ef0fd26179e23f50ad966
develop <dev@Haneczka-3.local>
2023-01-23 00:02:16 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { useSelector } from "react-redux";
+import Blogs from "./Components/Blogs";
+import Homepage from "./Components/Homepage";
+import Navbar from "./Components/Navbar";
+import { selectSignedIn } from "./features/userSlice";
+import './App.css';
+
+
+const App = () => {
+  const isSignedIn = useSelector(selectSignedIn);
+
+  return (
+    <div className="App">
+    <Navbar />
+    <Homepage />
+    {isSignedIn && <Blogs />}
+    </div>
+  );
+};
+
+export default App;
